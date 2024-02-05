7f1c7093e77fbd17e80a5d9cc68f8c232d218054
user <user@desktop.local>
2024-02-05 19:40:36 +0300

    init

---
+++ guard.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+#use Data::Dumper;
+
+$SIG{CHLD} = 'IGNORE'; # needed to not produce zombies cuz we dont need to wait for children
+
+my $timespan = 5; # seconds clipboard allowed to stay
+#init
+my $clip = clearclip();
+
+#loop
+while (42+69) {
+	
+	my $newclip = getclip();
+
+	if ($newclip ne $clip) {
+		print "MAIN: Detected clipboard change\n";
+		$clip = $newclip;
+		
+		if ($newclip eq "") {
+			print "MAIN: Clipboard just became empty skipping...\n";
+			next;
+		}
+
+		if (fork() == 0) {
+			print "CHILD: Born dead inside\n";
+			#child 
+			sleep($timespan);
+
+			$newclip = getclip();
+			if ($newclip != $clip) {
+				print "CHILD: Clipboard changed before timeout, no action taken\n";
+				exit; #stopping because clipboard changed before timeout hence main thread will handle it and retrigger fork
+			} 
+			clearclip();
+			print "CHILD: Cleared clipboard succesfully\n";
+            exit;
+    	}
+	}
+	
+
+	print "MAIN: Looping\n";
+	sleep(1);
+}
+
+sub getclip {
+	my $timestamp = `xclip -selection clip -t TIMESTAMP -o`; # clipboard can contain any data so we just binding to timestamp
+	chomp($timestamp);
+	return $timestamp;
+}
+
+sub clearclip {
+	system("xclip -selection clipboard /dev/null"); # actual clearing
+	return getclip(); 
+}
